94fc1d8f63b482d5beadd94d91b364371cfeff16
Stefan Arentz <stefan@arentz.ca>
2014-11-01 20:41:06 -0400

    Added tests for Hawk code

--- fxa/hawk_test.go
+++ fxa/hawk_test.go
@@ -5,8 +5,11 @@
 package fxa
 
 import (
+	"net/http"
 	"net/url"
+	"strings"
 	"testing"
+	"time"
 )
 
 func Test_portForURL_HTTP(t *testing.T) {
@@ -27,5 +30,38 @@ func Test_portForURL_Custom(t *testing.T) {
 	u, _ := url.Parse("http://www.example.com:8080/index.html")
 	if port, err := portForURL(u); err != nil || port != 8080 {
 		t.Error()
+	}
+}
+
+func Test_hawkPayloadHash(t *testing.T) {
+	body := "Thank you for flying Hawk"
+	request, _ := http.NewRequest("POST", "http://example.com:8000/resource/1?b=1&a=2", strings.NewReader(body))
+	request.Header.Set("Content-Type", "text/plain")
+	if hash, err := hawkPayloadHash(request, strings.NewReader(body)); err != nil || hash != "Yi9LfIIFRtBEPt74PVmbTF/xVAwPn7ub15ePICfgnuY=" {
+		t.Error()
+	}
+}
+
+func Test_hawkSignature_GET(t *testing.T) {
+	request, _ := http.NewRequest("GET", "http://example.com:8000/resource/1?b=1&a=2", nil)
+	signature, err := hawkSignature(request, "", []byte("werxhqb98rpaxn39848xrunpaw3489ruxnpa98w4rxn"), time.Unix(1353832234, 0), "j4h3g2", "some-app-ext-data")
+	if err != nil || signature != "6R4rV5iE+NPoym+WwjeHzjAGXUtLNIxmo1vpMofpLAE=" {
+		t.Error("Request signature failure: ", err, signature)
+	}
+}
+
+func Test_hawkSignature_POST(t *testing.T) {
+	body := "Thank you for flying Hawk"
+	request, _ := http.NewRequest("POST", "http://example.com:8000/resource/1?b=1&a=2", strings.NewReader(body))
+	request.Header.Set("Content-Type", "text/plain")
+
+	hash, err := hawkPayloadHash(request, strings.NewReader(body))
+	if err != nil || hash != "Yi9LfIIFRtBEPt74PVmbTF/xVAwPn7ub15ePICfgnuY=" {
+		t.Error("Payload hash failure")
+	}
+
+	signature, err := hawkSignature(request, hash, []byte("werxhqb98rpaxn39848xrunpaw3489ruxnpa98w4rxn"), time.Unix(1353832234, 0), "j4h3g2", "some-app-ext-data")
+	if err != nil || signature != "aSe1DERmZuRl3pI36/9BdZmnErTw3sNzOOAUlfeKjVw=" {
+		t.Error("Request signature failure: ", err, signature)
 	}
 }
